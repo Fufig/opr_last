9d29ff9b628ed13e3d0fa216d52bc02e89f4f76d
dima <blackbonik@gmail.com>
2025-05-18 10:07:56 -0700

    f

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -15,7 +15,7 @@ def create_app(config_class=Config):
 
     CORS(app, 
          resources={r"/api/*": {
-             "origins": ["http://localhost:5173", "http://127.0.0.1:5173"],
+             "origins": ["http://localhost:3000", "http://127.0.0.1:3000"],
              "supports_credentials": True,
              "allow_credentials": True,
              "allow_headers": ["Content-Type", "Authorization"],
